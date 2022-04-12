7b26ca5017101da024e31d4aed03db1960e5dcbf
IgorTeplych <91873250+IgorTeplych@users.noreply.github.com>
2022-04-12 13:46:36 +0600

    Добавил файлы

---
+++ HM3/Handlers/ExceptionHandler.cs
@@ -0,0 +1,17 @@
+﻿using HM3.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM3.Handlers
+{
+    public class ExceptionHandler
+    {
+        public void Handle(IRunCommand)
+        {
+
+        }
+    }
+}
---
+++ HM3/Works/Run.cs
@@ -0,0 +1,35 @@
+﻿using HM3.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM3.Works
+{
+    public class Run
+    {
+        Queue queue = new Queue();
+        public void Start()
+        {
+            while (!queue.IsFree())
+            {
+                try
+                {
+                    queue.Get().Execute();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+
+
+        public void AddCommand(IRunCommand runCommand)
+        {
+            queue.Add(runCommand);
+        }
+    }
+}
